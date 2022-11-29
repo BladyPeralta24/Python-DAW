d87dc4e4a3ce893bdf6eb92c1484442ac65cb0d5
Bladimir <bladimir.peralta@iescesarmanrique.com>
2022-11-29 09:50:12 +0000

    productor vectorial iniciado

---
+++ producto_vectorial.py
@@ -0,0 +1,5 @@
+vector1 = []
+vector2 = []
+coordenada_i = input("Inserte la coordenada X del primer vector")
+coordenada_j = input("Inserte la coordenada Y del primer vector")
+coordenada_k = input("Inserte la coordenada Z del primer vector")
