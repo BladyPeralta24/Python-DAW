1ee87af454342a39ce699ad58721842df1075bdd
bladimir <bladimir.peralta@iescesarmanrique.com>
2022-11-29 01:56:27 +0000

    nueva actualizacion de los trabajos

--- nota_media.py
+++ nota_media.py
@@ -5,33 +5,86 @@
 #
 #Por último, nos devolverá la nota media de los módulos.
 
-asignatura = []
-nota = []
-alumno = []
+modulo = []
+notas = []
 salir = False
-
 while not salir:
-    nombre_apellidos = input("Inserte el nombre y apellidos del alumno/a:\n")
-    alumno.append(nombre_apellidos)
     
-    numero_asignatura = int(input("¿Cuantas asignaturas está cursando?: "))
+    asignatura = int(input("¿Cuantas asignaturas está cursando?: "))
     
+    # introduccion de las asignaturas para el usuario
     i = 0
-    while i < numero_asignatura: 
-        if numero_asignatura > 0:
-            for asignaturas in range(numero_asignatura):
+    while i < asignatura: 
+        if asignatura > 0:
+            for i in range(asignatura):
                 nombre_asignatura = input("Introduce la asignatura que está cursando: ")
-                asignatura.append(nombre_asignatura)
-                numero_asignatura -= 1
+                modulo.append(nombre_asignatura)
+                asignatura -= 1
         
         i += 1
-                
-    salir = True
+        
+    print (modulo)
+    
+    
+    #Introducir las notas de cada asignatura para un alumno
+    suma_nota = 0
+    for i in range(len(modulo)):
+        nota_mod = input("Introduce la nota de " + modulo[i] + " : ")
+        notas.append(nota_mod)
+        print ("La nota de " + modulo[asignatura] + ", es de: "+ nota_mod)
+        suma_nota += int(nota_mod)
+        asignatura -= 1
+        i += 1
+        
+    print (notas)
+    
+    # Sacar la nota media de los modulos insertados
+    nota_media = suma_nota / int(len(notas))
+    
+    print("La nota media de los modulos es: " + str(nota_media))
+        
+        
+    # Salir del programa si el usuario quiere    
+    fin = input("¿Vas a introducir mas datos?(S/N): ")
+    if fin == "S" or fin == "s":
+        salir = False
+    elif fin == "N" or fin == "n":
+        salir = True
+                                          
+print (modulo)
+print (notas)
+print (nota_media)
 
-                                
-                
-                
-                
-                
-print (alumno)
-print (asignatura)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO:Implementacion del codigo para pruebas.
+##modulo = []
+##for i in [0, 1, 2]:
+##    asignatura = input("añade una asignatura a la lista: ")
+##    modulo.append(asignatura)
+##    i += 1
+##print(modulo)
+##
+##notas = []
+##i = 0
+##for i in [0, 1, 2]:
+##    nota_mod = input("Introduce la nota de " + modulo[i] + " : ")
+##    notas.append(nota_mod)
+##    print("La nota de " + modulo[i] + ", es de: "+ nota_mod)
+##    i += 1
+##print (notas)
+##
+##nota_media = int(int(notas[0]) + int(notas[1]) + int(notas[2])) / int(len(notas))
+##
+##print ("La nota media del curso es: " + str(nota_media))
