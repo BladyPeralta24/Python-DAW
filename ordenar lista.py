4299e9836e85f1bed6152287a3910b8a2fbe901b
Bladimir <bladimir.peralta@iescesarmanrique.com>
2022-12-02 11:54:54 +0000

    nuevos cambios de la lista ordenada

--- ordenar lista.py
+++ ordenar lista.py
@@ -17,3 +17,48 @@ for i in range(len(lista_ordenada)):
             lista_ordenada[j] = aux
     
 print(lista_ordenada)
+
+
+##  Ejercicio resuelto por el Profesor  ##
+
+""" 
+[Listas] Ordenar Lista
+Cree un algoritmo que solicite al usuario una lista de números enteros y luego la ordene de menor a mayor. 
+
+**Para la realización del ejercicio, no se permite el uso de funciones de ordenación de elementos.
+"""
+
+
+#numeros_enteros = []
+#
+#seguir = True
+#
+#
+#while seguir:
+#    
+#    nuevo_entero = input("Introduzca el siguiente elemento de la lista o deje vacío para finalizar: \n")
+#    if nuevo_entero.replace('-','').isnumeric():
+#        numeros_enteros.append(int(nuevo_entero))
+#    else:
+#        seguir = False
+#        
+#        
+#i = 0
+#j = 0
+#
+#
+#while i < len(numeros_enteros):
+#    j = 0
+#    while j < len(numeros_enteros):
+#        
+#        if numeros_enteros[i] > numeros_enteros[j]:
+#            aux = numeros_enteros[i]
+#            
+#            numeros_enteros[i] = numeros_enteros[j]
+#            numeros_enteros[j] = aux
+#            
+#        
+#        
+#        j += 1
+#    
+#    i += 1
