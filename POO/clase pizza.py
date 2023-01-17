5b7437f353501c1b3211eb8ffc03bb4376c06b5d
bladimir <bladimir.peralta@iescesarmanrique.com>
2023-01-17 23:51:01 +0000

    clase pizza terminada

--- POO/clase pizza.py
+++ POO/clase pizza.py
@@ -1,22 +1,19 @@
 class Pizza():
     def __init__(self, tamanho):
-        self.__ingredientes = []
+        self.__ingredientes = ['salsa de tomate', 'queso', 'oregano']
         self.tamanho = tamanho
         
         
-    def agregar_ingrediente(self):
-        ingredientes = ''
-        ingredientes = input('Introduce un ingrediente a la pizza: ')
-        while not ingredientes == '':
-            self.__ingredientes.append(ingredientes)
-            ingredientes = input('Introduce un ingrediente a la pizza: ')
+    def agregar_ingrediente(self, ingrediente):
         
-    def quitar_ingrediente(self):
-        ingrediente = input('¿Qué ingrediente deseas eliminar?: ')
-        while not ingrediente == '':
-            if ingrediente in self.__ingredientes:
-                self.__ingredientes.remove(ingrediente)
-            ingrediente = input('¿Qué ingrediente deseas eliminar?: ')
+        self.__ingredientes.append(ingrediente)
+        
+    def quitar_ingrediente(self, ingrediente):
+        
+        if ingrediente in self.__ingredientes:
+            
+            self.__ingredientes.remove(ingrediente)
+    
     
     def mostrar_informacion(self):
         
@@ -25,9 +22,9 @@ class Pizza():
 
 pizza1 = Pizza('mediana')
 
-pizza1.agregar_ingrediente()
+pizza1.agregar_ingrediente('jamon')
 print(pizza1.mostrar_informacion())
 
-pizza1.quitar_ingrediente()
+pizza1.quitar_ingrediente('oregano')
 print(pizza1.mostrar_informacion())
 
