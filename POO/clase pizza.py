f254d5a1f4b3c8224fc810ae91ed9699df333cfa
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-01-18 08:36:03 +0000

    ejercicio terminado

--- POO/clase pizza.py
+++ POO/clase pizza.py
@@ -1,23 +1,23 @@
 class Pizza():
-    def __init__(self, tamanho):
-        self.__ingredientes = ['salsa de tomate', 'queso', 'oregano']
+    def __init__(self, tamanho, *argumentos_entrada):
+        self.lista_ingredientes = []
         self.tamanho = tamanho
         
         
     def agregar_ingrediente(self, ingrediente):
         
-        self.__ingredientes.append(ingrediente)
+        self.lista_ingredientes.append(ingrediente)
         
     def quitar_ingrediente(self, ingrediente):
         
-        if ingrediente in self.__ingredientes:
+        if ingrediente in self.lista_ingredientes:
             
-            self.__ingredientes.remove(ingrediente)
+            self.lista_ingredientes.remove(ingrediente)
     
     
     def mostrar_informacion(self):
         
-        return (f'Ingredientes: {self.__ingredientes}, tamaño de la pizza, {self.tamanho}')
+        return (f'Ingredientes: {self.lista_ingredientes}, tamaño de la pizza, {self.tamanho}')
     
 
 pizza1 = Pizza('mediana')
@@ -27,4 +27,81 @@ print(pizza1.mostrar_informacion())
 
 pizza1.quitar_ingrediente('oregano')
 print(pizza1.mostrar_informacion())
+
+
+
+
+# Ejercicio hecho por el profesor
+"""
+   
+Clase Pizza
+Crea una clase "Pizza" con atributos "ingredientes" y "tamaño" y métodos "agregar_ingrediente", "quitar_ingrediente" y
+"mostrar_ingredientes". El método "agregar_ingrediente" debe añadir un ingrediente a la lista de ingredientes de la pizza, 
+el método "quitar_ingrediente" debe eliminar un ingrediente de la lista de ingredientes de la pizza y el método 
+"mostrar_ingredientes" debe imprimir todos los ingredientes de la pizza.    
+"""
+
+
+# class Pizza():
+    
+    
+#     def __init__(self,*argumentos_entrada):
+#         self.lista_ingredientes = []
+#         self.tamanho = 0
+        
+#         for ingrediente in argumentos_entrada:           
+#             self.agregar_ingrediente(ingrediente)
+        
+        
+#     def agregar_ingrediente(self,*args):
+        
+#         for ingrediente in args:
+#             if self.lista_ingredientes.count(ingrediente) == 0:
+#                 self.lista_ingredientes.append(ingrediente)
+#                 self.tamanho += 1
+                
+                
+#     def quitar_ingrediente(self,*args):
+        
+#         for ingrediente in args:
+#             if self.lista_ingredientes.count(ingrediente) > 0:
+#                 self.lista_ingredientes.remove(ingrediente)
+#                 self.tamanho -= 1
+                
+#     def mostrar_ingredientes(self):
+        
+        
+#         definicion_pizza = f"La pizza tiene un tamaño de {self.tamanho} y está creada con los ingredientes: "
+        
+#         for ingrediente in self.lista_ingredientes:            
+#             definicion_pizza += "\n - " + ingrediente
+            
+#         return definicion_pizza
+            
+        
+# marinera = Pizza("Gambas", "atún", "queso","tomate")
+
+
+# print(marinera.mostrar_ingredientes())
+
+# marinera.quitar_ingrediente("Gambas", "atún")
+
+# print(marinera.mostrar_ingredientes())
+
+# marinera.agregar_ingrediente("Gambas", "atún")
+
+# print(marinera.mostrar_ingredientes())
+
+
+
+
+
+
+
+
+
+
+
+
+
 
