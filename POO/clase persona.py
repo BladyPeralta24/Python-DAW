c1f69ffaccb556b1970871efaf2520554b6af647
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-01-20 11:55:06 +0000

    ejercicio de clase persona completada

--- POO/clase persona.py
+++ POO/clase persona.py
@@ -43,9 +43,9 @@ class Persona():
     
     def esMayorDeEdad(self):
         if self.__edad >= 18:
-            return "Es mayor de edad"
+            return "Es legal"
         else:
-            return "Es menor de edad"    
+            return "No es legal"    
             
             
 
@@ -56,3 +56,86 @@ persona1.edad = 24
 persona1.dni = '78848952F'
 print(persona1.mostrar())
 print(persona1.esMayorDeEdad())
+
+
+
+
+
+
+
+# ejercicio hecho por el profesor.
+
+# from documento import Documento
+
+# class Persona():
+    
+#     def __init__(self, nombre: str = '', edad: int = 18, nif: str = ''):
+#         self.nombre = nombre
+#         self.edad   = edad
+#         self.nif    = nif
+        
+        
+        
+#     @property
+#     def nif(self):
+#         return "NIF: " + self.__nif
+    
+    
+#     @nif.setter
+#     def nif(self, nuevo_valor):
+        
+#         doc = Documento()
+        
+#         if doc.nif_valido(nuevo_valor):
+#             self.__nif = nuevo_valor
+#         else:
+#             print("[ERROR] el NIF insertado no es válido")
+#             exit()
+    
+#     @property
+#     def edad(self):
+#         return self.__edad
+    
+#     @edad.setter
+#     def edad(self, nuevo_valor):
+        
+#         if isinstance(nuevo_valor, int) and nuevo_valor >= 0 and nuevo_valor <= 120:
+#             self.__edad = nuevo_valor
+#         else:
+#             print("[ERROR] la edad insertada no es válido")
+#             exit()
+            
+            
+#     def mostrar(self):
+#         return f"Nombre: {self.nombre} Edad: {self.__edad} {self.nif} "
+    
+    
+#     def es_mayor_de_edad(self):       
+#         return self.__edad >= 18
+    
+# andres = Persona("Andrés",22,"78551004R")
+
+# print(andres.mostrar())
+
+
+# #print(andres.nif)
+
+
+
+# meter esta clase en otro documento
+# class Documento():
+    
+#     def __init__(self) -> None:
+#         pass
+
+
+#     def nif_valido(self, nif):
+#         letrasDNI = 'TRWAGMYFPDXBNJZSQVHLCKE'
+#         if len(nif) == 9:
+#             dni = nif[0:-1]
+#             if dni.isnumeric():
+#                 posLetra = int(dni)%23
+#                 if nif[-1].upper() == letrasDNI[posLetra]:
+#                     return True
+
+#         return False    
