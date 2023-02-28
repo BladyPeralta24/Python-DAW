f07cc72f63a109ed27726183018a6b36714ee55d
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-02-28 08:44:35 +0000

    implementaciones en la clase fraccion

--- POO/clase_fraccion.py
+++ POO/clase_fraccion.py
@@ -69,3 +69,61 @@ print(fraccion1 * fraccion2)
 print(fraccion1 / fraccion2)
 print(fraccion1 == fraccion2)
 print(fraccion1 != fraccion2)
+
+
+
+
+
+# Ejercicio hecho por el profe
+
+# class Fraccion():
+#     def __init__(self,numerador: int, denominador: int):
+#         mcd = math.gcd(numerador,denominador)
+        
+        
+#         self.numerador =  int(numerador / mcd)
+#         self.denominador =  int(denominador / mcd)
+
+
+#     def __str__(self):
+#         return str(self.numerador) + "\n-\n" + str(self.denominador)
+    
+    
+#     def __add__(self, fraccion2):
+#         numerador   = self.numerador * fraccion2.denominador + fraccion2.numerador * self.denominador
+#         denominador = self.denominador * fraccion2.denominador
+        
+#         return Fraccion(numerador, denominador)
+    
+#     def __sub__(self, fraccion2):
+#         numerador   = self.numerador * fraccion2.denominador - fraccion2.numerador * self.denominador
+#         denominador = self.denominador * fraccion2.denominador
+        
+#         return Fraccion(numerador, denominador)
+    
+    
+#     def __mul__(self, fraccion2);
+#         numerador   = self.numerador * fraccion2.numerador
+#         denominador = self.denominador * fraccion2.denominador
+        
+#         return Fraccion(numerador, denominador)
+    
+#     def __truediv__(self, fraccion2);
+#         numerador   = self.numerador * fraccion2.denominador
+#         denominador = self.denominador * fraccion2.numerador
+        
+#         return Fraccion(numerador, denominador)
+    
+    
+#     def __eq__(self, fraccion2):
+        
+#         return self.numerador == fraccion2.numerador and self.denominador == fraccion2.denominador
+    
+    
+#     def __ne__(self, fraccion2):
+        
+#         return self.numerador != fraccion2.numerador or self.denominador != fraccion2.denominador
+    
+# fraccion1 = Fraccion(2,4)
+# fraccion2 = Fraccion(1,4)
+# fraccion3 = Fraccion(1,4)
