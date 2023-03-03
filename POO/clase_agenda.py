2a10da7aafb966666b7c5be50255fec625cf4026
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-03-03 13:58:50 +0000

    Terminar clase agenda en casa

---
+++ POO/clase_agenda.py
@@ -0,0 +1,37 @@
+from clase_persona import Persona
+from clase_contacto import Contacto
+
+class Agenda():
+    
+    def __init__(self):
+        pass
+    
+    
+    def anhadir_contacto(self):
+        """ Solicitar todos los datos del contacto y lo añade a tu gestion """
+        pass
+    
+    def lista_contacto(self):
+        """ Contactos ordenados por su nombre alfabéticamente """
+        pass
+    
+    def buscar_contacto(self):
+        """ Buscar contactos por cualquiera de sus parámetros """
+        pass
+    
+    def editar_contacto(self):
+        """ Gestionar un menú para editar los parámetros del contacto, cualquiera de ellas """
+        pass
+    
+    
+    
+print(""" -- Menú de Agenda --
+Elija una de estas opciones:
+***********************************
+* 1. Añadir contacto              *
+* 2. Lista de contactos           *
+* 3. Busqueda de contacto         *
+* 4. Editar contacto              *
+***********************************      
+      
+""")
