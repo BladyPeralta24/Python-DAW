05f146ccefffde7bd52de5a2aa97393ec29426d1
bladimir <bladimir.peralta@iescesarmanrique.com>
2023-03-09 00:43:24 +0000

    clase agenda aun falta por implementar

--- POO/clase_agenda.py
+++ POO/clase_agenda.py
@@ -1,5 +1,39 @@
 from clase_contacto import Contacto
 
+"""crear funciones de validar nif y email para la clase"""
+
+def validarNIF (nif: str):
+    ValidarNIF = 'TRWAGMYFPDXBNJZSQVHLCKE'
+    nif = nif.upper()
+    if not nif[0:-1].isnumeric():
+        return ("Has introducido mal el NIF. Por favor introduce de nuevo el NIF: ")
+        
+    dni = int(nif[0:-1])
+    
+    posLetra = dni % 23
+    if ValidarNIF[posLetra] == nif[-1]:
+        return True
+    else:
+        return False
+
+
+def validarEmail(correo):
+    posArroba = int(0)
+    emailValido = False
+    
+    if not emailValido:
+        if correo.find("@") > 0:
+            posArroba = correo.find("@")
+            if correo.find(".", posArroba, -1) < (len(correo) -1) and correo.find(".", posArroba, -1) > posArroba and 1 < correo.find(".", posArroba, -1) - posArroba:
+                emailValido = True
+    
+    return emailValido
+
+
+
+
+
+
 class Agenda():
     
     def __init__(self):
@@ -16,7 +50,7 @@ class Agenda():
         edad = int(input('Introduce la edad de la persona: '))
         nif = input('Introduce el dni de la persona: ')
         direccion = input('Introduce la direccion del contacto: ')
-        telefono_fijo = input('Introduce un telefono fijo para el contacto:' )
+        telefono_fijo = input('Introduce un telefono fijo para el contacto: ' )
         telefono_movil = input('Introduce un telefono movíl para el contacto: ')
         email = input('Introduce en E-mail para el contacto: ')
         
@@ -37,67 +71,86 @@ class Agenda():
     
     def buscar_contacto(self):
         """ Buscar contactos por cualquiera de sus parámetros """
-        
-        busqueda = input("Introduce el valor del campo a buscar: ")
-        
-        encontrado = {}
-        
-        for nombre, contacto in self.agenda.items():
+        parametro = input("Introduce un parametro del usuario que deseas buscar: ")
+        for nombre, Contacto in self.agenda.items():
+            if parametro in Contacto.nombre:
+                print (Contacto.nombre, Contacto)
             
-            if busqueda in nombre:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.primer_apellido:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.nif:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.email:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.segundo_apellido:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.edad:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.direccion:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.telefono_fijo:
-                encontrado[nombre] = contacto
-                
-            if busqueda in contacto.telefono_movil:
-                encontrado[nombre] = contacto
-                
+            if parametro in Contacto.primer_apellido:
+                print (nombre, Contacto)
             
-            return encontrado
+            if parametro in Contacto.segundo_apellido:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.edad:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.direccion:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.telefono_fijo:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.telefono_movil:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.nif:
+                print (nombre, Contacto)
+            
+            if parametro in Contacto.email:
+                print (nombre, Contacto)
+        
     
     def editar_contacto(self):
         """ Gestionar un menú para editar los parámetros del contacto, cualquiera de ellas """
         
-        contacto_a_editar = input("Introduce el valor del contacto a editar: ")
-        
-        while not(contacto_a_editar in self.agenda):
-            print('El contacto, no esta en la agenda.\n')
-            contacto_a_editar = input('Introduzca el valor del contacto a editar: ')
-            
-        campo_a_editar = input("Introduzca el valor del campo a editar:\n[N] editar nombre\n[A]Editar primer apellido\n[E]Editar Email\n[SA] Editar segundo apellido\n[TF] Editar telefono fijo\n[TM] Editar telefono móvil\n[D] Editar direccion\n: ")
-        while not(campo_a_editar in ['N', 'A', 'E']):
-            print('El campo no se encuentra disponible.')
-            campo_a_editar = input("Introduzca el valor del campo a editar:\n[N] editar nombre\n[A]Editar primer apellido\n[E]Editar Email")
+        contacto_editar = input("Introduce el nombre del contacto que quieras editar: ")
+        if contacto_editar in self.agenda.keys():
+            clave = input("Elije la opcion a editar:\nNombre -\nPrimer apellido -\nSegundo Apellido -\nEdad -\nNIF -\nDireccion -\nTelefono fijo -\nTelefono movil -\nEmail -\n: ")
+            if clave.lower() == 'nombre' or clave.lower() == "Primer apellido" or clave.lower() == "Segundo apellido" or clave.lower() == "edad" or clave.lower() == "nif" or clave.lower() == "direccion" or clave.lower() == "telefono fijo" or clave.lower() == "telefono movil" or clave.lower() == "email":
+                
+                nuevo_valor = input("Introduce el valor del campo: ")
 
-        contacto_seleccionado = self.agenda[contacto_a_editar]
-        
-        if campo_a_editar == 'N':
-            contacto_seleccionado.nombre = input('Introduce el nombre del contacto: ')
-            
-        if campo_a_editar == 'A':
-            contacto_seleccionado.primer_apellido = input('Introduce el primer apellido del contacto: ')
-            
-        if campo_a_editar == 'E':
-            contacto_seleccionado.email = input('Introduce el email del contacto: ')
+                for nombre, contacto in self.agenda.items():
+                    if contacto_editar == nombre:
+                        
+                        if clave.lower() == "nombre":
+                            contacto.nombre = nuevo_valor
+
+                        if clave.lower() == "primer apellido":
+                            contacto.primer_apellido = nuevo_valor
+
+                        if clave.lower() == "Segundo apellido":
+                            contacto.segundo_apellido = nuevo_valor
+
+                        if clave.lower() == "nif":
+                            while not validarNIF(nuevo_valor):
+                                clave = input("Introduzca un NIF valido: ")
+                            contacto.nif = nuevo_valor
+
+                        if clave.lower() == "email":
+                            while not validarEmail(nuevo_valor):
+                                clave = input("Introduzca un Email válido: ")
+                            contacto.email = nuevo_valor
+
+                        if clave.lower() == "edad":
+                            contacto.edad = int(nuevo_valor)
+
+                        if clave.lower() == "direccion":
+                            contacto.direccion = nuevo_valor
+
+                        if clave.lower() == "telefono fijo":
+                            contacto.telefono_fijo = nuevo_valor
+
+                        if clave.lower() == "telefono movil":
+                            contacto.telefono_movil = nuevo_valor
+                            
+                    print ("El contacto se ha actualizado correctamente.\n")
+            else:
+                
+                print('Error. Este campo no existe.\n')
+        else:
+            print('Error, este contacto no existe.\n')
     
     
     
@@ -116,4 +169,4 @@ agenda = Agenda()
 
 agenda.anhadir_contacto()
 
-print(agenda.editar_contacto())
+agenda.buscar_contacto()
