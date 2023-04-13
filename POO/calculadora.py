d4a6763782234946485326fe270fb192e7f1edd2
bladimir <bladimir.peralta@iescesarmanrique.com>
2023-04-14 00:52:13 +0100

    ejercicio de la calculadora terminada

--- POO/calculadora.py
+++ POO/calculadora.py
@@ -6,32 +6,76 @@ class EntradaNoValidaError(Exception):
 
 
 class Calculadora():
-
+    
+    
+    @staticmethod
+    def verificarDatos(numero1, numero2):
+        if isinstance(numero1, int) and isinstance(numero2, int):
+            pass
+        else:
+            raise EntradaNoValidaError("Debes de introducir datos numéricos")
+    
+    
     @staticmethod
     def sumar(sumando1, sumando2):
-        if not isinstance(sumando1, int) and not isinstance(sumando2, int):
-            raise EntradaNoValidaError("debes de introducir un dato numerico")
-        else:
+        if not Calculadora.verificarDatos(sumando1, sumando2):
             return sumando1 + sumando2
+            
     
     @staticmethod
     def restar(minuendo, sustraendo):
-        return minuendo - sustraendo
+        if not Calculadora.verificarDatos(minuendo, sustraendo):
+            return minuendo - sustraendo
     
     @staticmethod
     def multiplicar(multiplicando, multiplicador):
-        return multiplicando * multiplicador
+        if not Calculadora.verificarDatos(multiplicando, multiplicador):
+            return multiplicando * multiplicador
     
     @staticmethod
-    def dividir(dividendo, divisor):
-        return dividendo / divisor
     
+    def dividir(dividendo, divisor):
+        if not Calculadora.verificarDatos(dividendo, divisor) and divisor != 0:
+            return dividendo / divisor
+        else:
+            raise DivisionPorCeroError("No se puede dividir por cero")
+        
+        
 
+prueba = Calculadora()
 
 try:
-    calculo1 = Calculadora()
 
-    print(calculo1.sumar(3,"g"))
+    print(prueba.sumar(3,5))
     
 except EntradaNoValidaError as e:
-    print("Error al introducir un dato no numerico." + e)
+    print("Error." , e)
+    
+try:
+
+    print(prueba.restar(3,5))
+    
+except EntradaNoValidaError as e:
+    print("Error." , e)
+    
+try:
+
+    print(prueba.multiplicar(3,5))
+    
+except EntradaNoValidaError as e:
+    print("Error." , e)
+    
+try:
+
+    print(prueba.dividir(3,5))
+    
+except EntradaNoValidaError as e:
+    print("Error." , e)
+    
+try:
+
+    print(prueba.dividir(3,0))
+    
+except DivisionPorCeroError as e:
+    print("Error." , e)
+    
