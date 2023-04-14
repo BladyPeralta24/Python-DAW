ad28d21e0221394e01c37ac08e3a5471101048a3
Bladimir <bladimir.peralta@iescesarmanrique.com>
2023-04-14 13:22:05 +0100

    expresion regular E-mail terminado

---
+++ POO/e-mail.py
@@ -0,0 +1,22 @@
+import re
+
+
+# Crea la expresión regular para validar un email. 
+# El email estará construido por:
+# texto + @ + texto + . (un punto) + texto (máximo 3 caracteres).
+
+
+def validar_email (correo):
+    emailValido = False
+    
+    if not re.findall('.+[@].+\..[a-z]{1,2}$', correo):
+        return emailValido
+    else:
+        return True
+
+
+# prueba de correo: bladimir.peralta@iescesarmanrique.es
+print(validar_email("bladimir.peralta@iescesarmanrique.es"))
+
+
+#print(re.findall('.+[@].+\..[a-z]{1,2}$', "bladimir.peralta@iescesarmanrique.com"))
